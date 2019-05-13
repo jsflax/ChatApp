f78f86521121bf75dcf20c0c583b27251a1571f9
Jason Flax <jsflax@gmail.com>
2019-05-13 22:30:35 +0300

    add gitignore

--- src/index.js
+++ src/index.js
@@ -32,6 +32,9 @@ async function main() {
 	library.add(faPaperPlane, faEllipsisV);
 	const rootElement = document.getElementById("root");
 
+	if (!client.auth.isLoggedIn && client.auth.listUsers().length != 0) {
+		await client.auth.removeUserWithId(client.auth.listUsers()[0].id);
+	}
 	if (client.auth.isLoggedIn) {
 		ReactDOM.render(<ChatRoom />, rootElement);	
 	} else {
